7b91a56cc7d326460eeacc60cc90734ac224eaa0
dcadata <39072469+dcadata@users.noreply.github.com>
2020-01-07 23:07:08 -0500

    check if party exists

--- scrapers.py
+++ scrapers.py
@@ -406,7 +406,7 @@ class CandidateRowScraper:
 
             if self.candidate_cell.text:
                 candidate_items = [i.strip() for i in self.candidate_cell.text.strip().split('\n')]
-                self.name, party, winner = (candidate_items + [None, None, None])[:3]
+                self.name, self.party, winner = (candidate_items + [None, None, None])[:3]
 
                 if self.name.endswith('*'):
                     self.incumbency = True
@@ -414,7 +414,8 @@ class CandidateRowScraper:
                 else:
                     self.incumbency = False
 
-                self.party = party.replace('(', '').replace(')', '').strip()
+                if self.party:
+                    self.party = self.party.replace('(', '').replace(')', '').strip()
 
     @abstractmethod
     def _get_remaining_cells(self):
