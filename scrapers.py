396351127db867a003f2d05b65c17f032069b175
dcadata <39072469+dcadata@users.noreply.github.com>
2020-01-12 14:50:19 -0500

    move comments

--- scrapers.py
+++ scrapers.py
@@ -219,11 +219,11 @@ class CandidateScraper(Requester):
             self.has_ie = bool(self.sidebar_menu.find('li', text='Independent Expenditures'))
 
     def _get_current_election_data(self):
+        # comment in HTML for this elem: shows the next upcoming election, unless there was an election recently,
+        # in which case it displays the results
         show_all_elections_link_elem = self.soup.find('a', text=lambda x: 'Show all elections for' in str(x))
         if show_all_elections_link_elem:
             current_election_elem = show_all_elections_link_elem.find_parent('div', class_='panel-body')
-        # comment in HTML for this elem: shows the next upcoming election, unless there was an election recently,
-        # in which case it displays the results
             self.__dict__.update(CandidateCurrentElectionScraper(current_election_elem, self.name).result)
 
 class CandidateCurrentElectionScraper:
